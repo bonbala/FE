4ca5a9fc76996c8869b41ba04f59f74dea01e793
bonbala <nhathieu1805@gmail.com>
2024-08-14 19:14:44 +0700

    bt resposive

---
+++ Code/7-8/main.js
@@ -0,0 +1,18 @@
+console.log("Hello")
+var FMA = "Peak Anime"
+var MHA = "Fk Anime"
+
+var fullName = "Nhat Hieu"
+var phoneNumber = "123465789"
+var address  ="KTX"
+var number = 2
+var isGay = false;
+console.log('fullName :' ,fullName);
+console.log('phoneNumber:', phoneNumber)
+console.log('address:', address)
+
+var firstNumber = '2';
+var secondNumber= 23;
+
+console.log('tong:', firstNumber + secondNumber)
+console.log('hieu:',firstNumber - secondNumber)
